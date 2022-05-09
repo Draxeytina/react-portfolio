34f9f1c49d449e9b8e3f3796296e941223cc1bee
Draxeytina <tmurambinda01@gmail.com>
2022-05-10 01:51:00 +0200

    replace var with let js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 function myFunction() {
   // make mobile nav list disappear and appear
-  var x = document.getElementById("menu-item");
+  let x = document.getElementById("menu-item");
   if (x.style.display === "flex") {
     x.style.display = "none";
   } else {
@@ -8,13 +8,13 @@ function myFunction() {
   }
 
   // set the hamburger icon to close when clicked
-  var y = document.getElementById("menu-item");
+  let y = document.getElementById("menu-item");
   if (y.className === "menu-items") {
       y.className += " responsive";
   } else {
       y.className = "menu-items";
   }
-  var icon=document.getElementsByClassName("fa-bars")[0];
+  let icon=document.getElementsByClassName("fa-bars")[0];
   if(icon){
      icon.className = icon.className.replace("fa-bars", "fa-times");
   }
@@ -24,7 +24,7 @@ function myFunction() {
   }
 
   // make the welcome text invisible when mobile menu appear
-  var z = document.getElementById("welcome-top");
+  let z = document.getElementById("welcome-top");
   if (z.style.display === "none") {
     z.style.display = "flex";
   } else {
