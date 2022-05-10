0c117890e377ef1bf8bdc499b9c5bcc728935b32
Draxeytina <tmurambinda01@gmail.com>
2022-05-10 02:02:13 +0200

    replace double qoutes

--- test.js
+++ test.js
@@ -1,33 +1,32 @@
 function myFunction() {
   // make mobile nav list disappear and appear
-  let x = document.getElementById("menu-item");
-  if (x.style.display === "flex") {
-    x.style.display = "none";
+  let z = document.getElementById('welcome-top');
+  let x = document.getElementById('menu-item');
+  if (x.style.display === 'flex') {
+    x.style.display = 'none';
   } else {
-    x.style.display = "flex";
+    x.style.display = 'flex';
   }
 
   // set the hamburger icon to close when clicked
-  let y = document.getElementById("menu-item");
-  if (y.className === "menu-items") {
-      y.className += " responsive";
+  let y = document.getElementById('menu-item');
+  if (y.className === 'menu-items') {
+      y.className += ' responsive';
   } else {
-      y.className = "menu-items";
+      y.className = 'menu-items';
   }
-  let icon=document.getElementsByClassName("fa-bars")[0];
-  if(icon){
-     icon.className = icon.className.replace("fa-bars", "fa-times");
-  }
-  else{
-  icon=document.getElementsByClassName("fa-times")[0];
-   icon.className = icon.className.replace("fa-times", "fa-bars");
+  let icon=document.getElementsByClassName('fa-bars')[0];
+  if (icon) {
+     icon.className = icon.className.replace('fa-bars', 'fa-times');
+  } else {
+  icon = document.getElementsByClassName('fa-times')[0];
+  icon.className = icon.className.replace('fa-times', 'fa-bars');
   }
 
   // make the welcome text invisible when mobile menu appear
-  let z = document.getElementById("welcome-top");
-  if (z.style.display === "none") {
-    z.style.display = "flex";
+  if (z.style.display === 'none') {
+    z.style.display = 'flex';
   } else {
-    z.style.display = "none";
+    z.style.display = 'none';
   }
 }
