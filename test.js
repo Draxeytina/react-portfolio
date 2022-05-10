3bfc1deb4cb85d040cb9c5be812f25fb0ef5bc9e
Draxeytina <tmurambinda01@gmail.com>
2022-05-10 02:09:45 +0200

    remove linter errors

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 function myFunction() {
   // make mobile nav list disappear and appear
-  let z = document.getElementById('welcome-top');
-  let x = document.getElementById('menu-item');
+  const z = document.getElementById('welcome-top');
+  const x = document.getElementById('menu-item');
   if (x.style.display === 'flex') {
     x.style.display = 'none';
   } else {
@@ -9,18 +9,18 @@ function myFunction() {
   }
 
   // set the hamburger icon to close when clicked
-  let y = document.getElementById('menu-item');
+  const y = document.getElementById('menu-item');
   if (y.className === 'menu-items') {
-      y.className += ' responsive';
+    y.className += ' responsive';
   } else {
-      y.className = 'menu-items';
+    y.className = 'menu-items';
   }
-  let icon=document.getElementsByClassName('fa-bars')[0];
+  let icon = document.getElementsByClassName('fa-bars')[0];
   if (icon) {
-     icon.className = icon.className.replace('fa-bars', 'fa-times');
+    icon.className = icon.className.replace('fa-bars', 'fa-times');
   } else {
-  icon = document.getElementsByClassName('fa-times')[0];
-  icon.className = icon.className.replace('fa-times', 'fa-bars');
+    icon = document.getElementsByClassName('fa-times')[0];
+    icon.className = icon.className.replace('fa-times', 'fa-bars');
   }
 
   // make the welcome text invisible when mobile menu appear
