1e48dc0b2a383b21dc8225bb74470649ada25e77
Draxeytina <tmurambinda01@gmail.com>
2022-05-12 01:26:26 +0200

    email validation

--- test.js
+++ test.js
@@ -215,3 +215,22 @@ projectBtns.forEach((button) => {
     };
   });
 });
+
+// validate lowercase on email field
+const form = document.getElementById('contact-form-id');
+const divError = document.getElementById('div-error');
+const errorMsg = document.getElementById('error-msg');
+const emailForm = document.getElementById('email-input');
+
+form.addEventListener('submit', (e) => {
+  if (emailForm.value.match(/^[a-z@.0-9-_]*$/)) {
+    divError.style.display = 'none';
+    errorMsg.innerHTML = '';
+    emailForm.style.border = '1px solid green';
+  } else {
+    e.preventDefault();
+    emailForm.style.border = '2px solid red';
+    divError.style.display = 'flex';
+    errorMsg.textContent = '*Email must be in lower case';
+  }
+});
